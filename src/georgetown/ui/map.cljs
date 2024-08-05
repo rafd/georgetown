e3693aeb24f7484faeee8c6c5d43054a500ac841
Rafal Dittwald <rafal.dittwald@gmail.com>
2024-08-05 17:41:47 -0400

    Highlight selected map tile

--- src/georgetown/ui/map.cljs
+++ src/georgetown/ui/map.cljs
@@ -17,34 +17,38 @@
           lot-count-height (inc y-range)]
       [:div {:style {:width (str (* lot-count-width tile-size) "em")
                      :height (str (* lot-count-height tile-size) "em")}}
-       (for [[y row-lots] (->> lots
-                               (sort-by (juxt :lot/x :lot/y))
-                               (group-by :lot/y)
-                               (sort-by key))]
-         ^{:key y}
-         [:div {:tw "flex"}
-          (for [lot row-lots
-                :let [deed (first (:deed/_lot lot))
-                      improvement (first (:improvement/_lot lot))]]
-            ^{:key (:lot/x lot)}
-            [:a {:href (pages/path-for [:page/lot {:id (:lot/id lot)}])
-                 :style {:width (str tile-size "em")
-                         :height (str tile-size "em")
-                         :background "green"
-                         :border-right "1px solid gray"
-                         :border-bottom "1px solid gray"
-                         :color "white"}}
-             (when deed
-               [:div {:tw "h-full block"
-                      :style {:border-width "2px"
-                              :border-style "solid"
-                              :border-color (->color (:user/id (:deed/owner deed)))}}
-                (when improvement
-                  [:div {:style {:font-size (str (/ tile-size 2) "em")
-                                 :text-align "center"
-                                 :pointer-events "none"
-                                 :line-height (str (/ tile-size 2) "em")}}
-                   (let [blueprint (schema/blueprints (:improvement/type improvement))]
-                     (:blueprint/icon blueprint))])])])])])))
+       (doall
+         (for [[y row-lots] (->> lots
+                                 (sort-by (juxt :lot/x :lot/y))
+                                 (group-by :lot/y)
+                                 (sort-by key))]
+           ^{:key y}
+           [:div {:tw "flex"}
+            (doall
+              (for [lot row-lots
+                    :let [deed (first (:deed/_lot lot))
+                          improvement (first (:improvement/_lot lot))]]
+                ^{:key (:lot/x lot)}
+                [:a {:href (pages/path-for [:page/lot {:id (:lot/id lot)}])
+                     :style {:width (str tile-size "em")
+                             :height (str tile-size "em")
+                             :background (if (pages/active? [:page/lot {:id (:lot/id lot)}])
+                                           "#026002"
+                                           "green")
+                             :border-right "1px solid gray"
+                             :border-bottom "1px solid gray"
+                             :color "white"}}
+                 (when deed
+                   [:div {:tw "h-full block"
+                          :style {:border-width "2px"
+                                  :border-style "solid"
+                                  :border-color (->color (:user/id (:deed/owner deed)))}}
+                    (when improvement
+                      [:div {:style {:font-size (str (/ tile-size 2) "em")
+                                     :text-align "center"
+                                     :pointer-events "none"
+                                     :line-height (str (/ tile-size 2) "em")}}
+                       (let [blueprint (schema/blueprints (:improvement/type improvement))]
+                         (:blueprint/icon blueprint))])])]))]))])))
 
 
