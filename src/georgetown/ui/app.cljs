226c9904eb84031c1cfe322574b20d954969d5dc
Rafal Dittwald <rafal.dittwald@gmail.com>
2024-08-05 17:37:51 -0400

    Move map-view into its own namespace

--- src/georgetown/ui/app.cljs
+++ src/georgetown/ui/app.cljs
@@ -1,55 +1,10 @@
 (ns georgetown.ui.app
   (:require
     [bloom.commons.pages :as pages]
-    [reagent.core :as r]
-    [georgetown.schema :as schema]
-    [georgetown.client.state :as state]))
-
-(def tile-size 4)
-
-(defn ->color [x]
-  (str "oklch(50% 70% " (hash x) ")"))
-
-(defn map-view []
-  (when-let [lots @state/lots]
-    (let [x-range (apply max-key :lot/x lots)
-          y-range (apply max-key :lot/y lots)
-          lot-count-width (inc x-range)
-          lot-count-height (inc y-range)]
-      [:div {:style {:width (str (* lot-count-width tile-size) "em")
-                     :height (str (* lot-count-height tile-size) "em")}}
-       (for [[y row-lots] (->> lots
-                               (sort-by (juxt :lot/x :lot/y))
-                               (group-by :lot/y)
-                               (sort-by key))]
-         ^{:key y}
-         [:div {:tw "flex"}
-          (for [lot row-lots
-                :let [deed (first (:deed/_lot lot))
-                      improvement (first (:improvement/_lot lot))]]
-            ^{:key (:lot/x lot)}
-            [:a {:href (pages/path-for [:page/lot {:id (:lot/id lot)}])
-                 :style {:width (str tile-size "em")
-                         :height (str tile-size "em")
-                         :background "green"
-                         :border-right "1px solid gray"
-                         :border-bottom "1px solid gray"
-                         :color "white"}}
-             (when deed
-               [:div {:tw "h-full block"
-                      :style {:border-width "2px"
-                              :border-style "solid"
-                              :border-color (->color (:user/id (:deed/owner deed)))}}
-                (when improvement
-                  [:div {:style {:font-size (str (/ tile-size 2) "em")
-                                 :text-align "center"
-                                 :pointer-events "none"
-                                 :line-height (str (/ tile-size 2) "em")}}
-                   (let [blueprint (schema/blueprints (:improvement/type improvement))]
-                     (:blueprint/icon blueprint))])])])])])))
+    [georgetown.ui.map :as map]))
 
 (defn app-view []
   [:div {:tw "flex"}
-   [map-view]
+   [map/map-view]
    [:div {:tw "p-2"}
     [pages/current-page-view]]])
