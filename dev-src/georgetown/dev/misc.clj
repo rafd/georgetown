a7216e6aae5c2ad8bacd3dde71d14d429840fbad
Rafal Dittwald <rafal.dittwald@gmail.com>
2025-12-18 18:39:22 -0500

    Add old helper scripts

--- dev-src/georgetown/dev/misc.clj
+++ dev-src/georgetown/dev/misc.clj
@@ -20,3 +20,32 @@
        (map (fn [e]
               [:db/retractEntity e]))
        (db/transact!)))
+
+(defn remove-bankrupt-players! []
+  (->> (db/q '[:find [?e ...]
+               :where
+               [?e :resident/id _]
+               [?e :resident/money-balance ?balance]
+               [(neg? ?balance)]])
+       (map (fn [e-id]
+              [:db/retractEntity e-id]))
+       (db/transact!)))
+
+(defn redominate-balances! []
+  (do
+   (->> (db/q '[:find ?e ?balance
+                :where
+                [?e :resident/money-balance ?balance]
+                [(< 1000 ?balance)]])
+        (map (fn [[ e-id balance]]
+               [:db/add e-id :resident/money-balance (int (/ balance 10))]))
+        (db/transact!))
+
+   (->> (db/q '[:find ?e ?balance
+                :where
+                [?e :island/citizen-money-balance ?balance]
+                [(< 1000 ?balance)]])
+        (map (fn [[ e-id balance]]
+               [:db/add e-id :island/citizen-money-balance (int (/ balance 10))]))
+        (db/transact!))))
+
