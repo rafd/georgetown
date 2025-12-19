160ead96fb73abdbf1b2d42458b0478bd6e789b6
Rafal Dittwald <rafal.dittwald@gmail.com>
2025-12-19 01:03:51 -0500

    Bump datalevin to fix issues in prod

--- project.clj
+++ project.clj
@@ -14,19 +14,16 @@
                  [applied-science/js-interop "0.4.2"]
 
                  ;; db
-                 [datalevin "0.9.5"]
+                 [datalevin "0.9.20"]
                  [com.taoensso/nippy "3.3.0"]
                  [io.airlift/aircompressor "0.26"]]
-  :jvm-opts [;; datalevin
-             "--add-opens=java.base/java.nio=ALL-UNNAMED"
-             "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED"]
   :main georgetown.core
   :plugins [[io.bloomventures/omni "0.34.0"]]
   :omni-config georgetown.omni-config/omni-config
   :profiles {:dev
              {:source-paths ["dev-src"]}
              :uberjar
-             {:aot :all
+             {:aot [georgetown.core]
               :prep-tasks ["javac"
                            ["omni" "compile"]
                            "compile"]}})
